7138dbc7c7331bf256ab9037071623e04042cacb
Alexander Andreev <alexandreew@mail.ru>
2021-07-19 22:49:58 +0700

    adedd gitignore

---
+++ main.java
@@ -0,0 +1,8 @@
+class Main {
+        public static void main(String[] args) {
+        int amout = 10000;
+        int bonus = 20;
+        int result = amout / bonus;
+        System.out.println(result);
+    }
+}
