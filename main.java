1ca166e074e84e76426cb74fce7427e66188716f
alexboom85 <46699662+alexboom85@users.noreply.github.com>
2021-07-20 14:08:46 +0700

    Delete main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-        public static void main(String[] args) {
-        int amout = 10000;
-        int bonus = 20;
-        int result = amout / bonus;
-        System.out.println(result);
-    }
-}
